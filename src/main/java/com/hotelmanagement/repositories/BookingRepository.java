e1774fafdd355058e209d5b237a31b65c2a668a0
luismpereirao <luismpereiraovejero@gmail.com>
2025-03-23 13:30:20 +0100

    created the docker file and repositories

---
+++ src/main/java/com/hotelmanagement/repositories/BookingRepository.java
@@ -0,0 +1,15 @@
+package com.hotelmanagement.repositories;
+
+import com.hotelmanagement.entities.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    List<Booking> findByRoomId(Long roomId);
+
+    List<Booking> findByBookingConfirmationCode(String confirmationCode);
+
+    List<Booking> findByUserId(Long userId);
+}
